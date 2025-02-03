4fb8a33c144f20d767c89a2015bf53f92114954d
somnath-tech <somnath@euphoriagenx.in>
2025-02-04 00:20:45 +0530

    user model

---
+++ backend/model/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const userSheama = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true
+    },
+    userEmail:{
+        type:String,
+        required:true
+    },
+    userPassword:{
+        type:String,
+        required:true
+    },
+    userAddress:{
+        type:String,
+        required:true
+    },
+    userPhone:{
+        type:Number,
+        required:true
+    },
+    userRole:{
+        type:String,
+        required:true
+    },
+
+},
+{
+    timestamps:true
+}
+
+)
